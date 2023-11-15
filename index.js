9b92be723778fe538a7b658e586160c7179e906a
Shally593 <shally.slr@gmail.com>
2023-11-15 18:01:36 +0700

    Update XI

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './Callapi';
+import App from './Editdata';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
