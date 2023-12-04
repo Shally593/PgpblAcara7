ba401726cac648c936ba621e9f8f7f7ea2536cfa
Shally593 <shally.slr@gmail.com>
2023-12-04 21:54:03 +0700

    Update
    responsi

--- Callapi.js
+++ Callapi.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Text, View, FlatList, SafeAreaView, StyleSheet} from 'react-native';
+import {Text, View, FlatList, SafeAreaView, StyleSheet,  Button, Alert, Linking} from 'react-native';
+import { TouchableOpacity } from 'react-native';
 // import { SafeAreaView } from 'react-native-safe-area-context'
 import {useEffect, useState} from 'react';
+
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const Callapi = () => {
   const jsonUrl =
-    'https://script.google.com/macros/s/AKfycbzeDMPto8t24hT0SXlejD8yc-KNZ1cC7wyDqLn0hZGUMDuMmFe8eNks6bKljxk0K94YuA/exec';
+    'https://script.google.com/macros/s/AKfycbwQ6Sm0fBFv1GVmohay5oEiFUuuLRrZQiSNSxDnacpbbwkMx04GhRceAzOQUs5J0KjT1w/exec';
   const [isLoading, setLoading] = useState(true);
   const [dataUser, setDataUser] = useState({});
   const [refresh, setRefresh] = useState(false);
@@ -33,8 +35,39 @@ const Callapi = () => {
       .finally(() => setLoading(false));
   }
 
+//   function deleteData(id) {
+//     fetch(jsonUrl + '/' + id, {
+//       method: 'DELETE',
+//     })
+//     .then((response) => response.json())
+//     .then((json) => {
+//       console.log(json);
+//           alert('Data terhapus');
+//           refreshPage();
+//         })
+//  } 
+
+//   const handleDelete = (id) => {
+//     // Panggil API atau layanan untuk menghapus data
+//     const deleteUrl = `https://script.google.com/macros/s/AKfycbzeDMPto8t24hT0SXlejD8yc-KNZ1cC7wyDqLn0hZGUMDuMmFe8eNks6bKljxk0K94YuA/exec/${id}`;
+
+//     fetch(deleteUrl, {
+//       method: 'DELETE',
+//     })
+//       .then(response => {
+//         if (response.ok) {
+//           // Data berhasil dihapus, perbarui tampilan dengan mengambil data baru
+//           refreshPage();
+//         } else {
+//           console.error('Gagal menghapus data');
+//         }
+//       })
+//       .catch(error => console.error(error));
+//   };
+
   return (
     <SafeAreaView>
+      {/* <Text style={styles.title}>List Data Pengguna</Text> */}
       {isLoading ? (
         <View>
           <Text>Loading...</Text>
@@ -49,20 +82,64 @@ const Callapi = () => {
             refreshing={refresh}
             keyExtractor={({id}, index) => id}
             renderItem={({item}) => (
+
+              // <View style={styles.card}>
+              // {/* Konten Data */}
+              // <TouchableOpacity onPress={() => handleDelete(item.id)}>
+              //   <FontAwesome5 name="trash" size={17} color="red" />
+              // </TouchableOpacity>
+
+              <View>
               <View style={styles.card}>
-                <View style={styles.avatar}>
-                  <FontAwesome5 name={item.icon} size={50} color={item.color} />
-                </View>
                 <View>
                   <Text style={styles.cardtitle}>{item.nama}</Text>
-                  <Text>Deskripsi : {item.deskripsi}</Text>
-                  <Text>Timestamps : {item.timestamp}</Text>
+                  <Text>Jenis : {item.jenis}</Text>
+                  <Text>Alamat : {item.alamat}</Text>
                   <Text>{item.latitude}, {item.longitude}</Text>
                 </View>
               </View>
+
+              {/* <View style={styles.form}>
+              <Button title="Hapus"
+                onPress={() => Alert.alert('Hapus data', 'Yakin akan menghapus data ini?', [
+                { text: 'Tidak', onPress: () => console.log('button tidak') },
+                { text: 'Ya', onPress: () => deleteData(item.id) },
+                ])}
+                color={'red'}
+                />
+              </View> */}
+
+              {/* <View >
+              <Button title = "Lokasi"
+                onPress={() => Linking.openURL ('google.navigation:q='+(item.latitude)+','+(item.longitude))}
+                color={'green'}
+                style={{ borderRadius: 50 }}
+                titleStyle={{ color: '#E2C799' }}
+                />
+              </View> */}
+
+            <View>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: 'green',
+                  borderRadius: 25,
+                  justifyContent : 'center',
+                  marginLeft : 15, 
+                  marginRight : 15,
+                  padding: 10, // Atur padding sesuai kebutuhan
+                }}
+                onPress={() => Linking.openURL('google.navigation:q=' + item.latitude + ',' + item.longitude)}
+              >
+                <Text style={{ color: 'white', textAlign: 'center', fontSize : 16, }}>Rute</Text>
+              </TouchableOpacity>
+            </View>
+
+
+            </View>
             )}
           />
         </View>
+        
       )}
     </SafeAreaView>
   );
@@ -73,11 +150,17 @@ export default Callapi;
 const styles = StyleSheet.create({
   title: {
     paddingVertical: 12,
-    backgroundColor: '#333',
+    backgroundColor: '#004225',
     color: 'white',
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
+    borderRadius : 25,
+    left : 20,
+    right : 20, 
+    top : 15,
+    justifyContent : 'center',
+    alignItems : 'center',
   },
   avatar: {
     borderRadius: 100,
@@ -86,7 +169,9 @@ const styles = StyleSheet.create({
   cardtitle: {
     fontSize: 15,
     fontWeight: 'bold',
+    color : 'black'
   },
+
   card: {
     flexDirection: 'row',
     padding: 20,
--- navigation/Petalokasiobjek.js
+++ navigation/Petalokasiobjek.js
@@ -2,9 +2,10 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 // import {useEffect, useState} from 'react';
-import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
+import {Text, View, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
 import Fontawesome5 from 'react-native-vector-icons/FontAwesome5';
 import Portofolio from '../App';
+import Teman from '../Getdata';
 import ListData from '../Callapi';
 import {WebView} from 'react-native-webview';
 
@@ -12,42 +13,88 @@ const Tab = createBottomTabNavigator();
 // const webmap = require('../Peta/map.html');
 // const [isLoading, setLoading] = useState(true);
 
-const formInput = 'https://shally593.github.io/pgpbl-12/';
-const webmap = 'https://shally593.github.io/pgpbl-12/map.html';
-// const jsonURL = 'https://script.google.com/macros/s/AKfycbzeDMPto8t24hT0SXlejD8yc-KNZ1cC7wyDqLn0hZGUMDuMmFe8eNks6bKljxk0K94YuA/exec'
+const formInput = 'https://shally593.github.io/pgpbl-responsi/';
+const webmap = 'https://shally593.github.io/pgpbl-responsi/map.html';
+
+// function HomeScreen() {
+//     return (
+//         <View>
+//             <Text style = {styles.title}>
+//                 APLIKASI PETA LOKASI OBJEK 
+//             </Text>
+//             <Text style = {{textAlign :'center'}}> Aplikasi nice tapi error dikit</Text>
+
+//             <View style = {styles.container}>
+//                 <Text style = {styles.text}>
+//                     Stack :
+//                 </Text>
+//                 <Text >
+//                     1. React Native 
+//                 </Text>
+//                 <Text >
+//                     2. HTML 
+//                 </Text >
+//                 <Text >
+//                     3. Leaflet JS 
+//                 </Text>
+//                 <Text >
+//                     4. Google Spreadsheet 
+//                 </Text>
+//                 <Text >
+//                     5. Font Awesome 
+//                 </Text>
+//                 <Text >
+//                     6. Github  
+//                 </Text>
+//             </View>
+//         </View>
+//     );
+// }
 
 function HomeScreen() {
     return (
-        <View >
-            <Text style = {styles.title}>
-                APLIKASI PETA LOKASI OBJEK 
-            </Text>
-            <Text style = {{textAlign :'center'}}> Aplikasi nice tapi error dikit</Text>
+        <ScrollView>
+            <View>
+                <Text style={styles.title}>GREEN PULSE APP</Text>
+                <Text style = {{textAlign :'center'}}> Temukan, Kolaborasi, dan Dukung Pengelolaan Sampah yang Berkelanjutan di Surabaya</Text>
+                <View style={styles.container}>
+                    <Text style={styles.text}>
+                        Green Pulse App merupakan aplikasi tematik berbasis spasial yang dirancang untuk memberikan informasi sebaran lokasi objek pengolahan sampak di Surabaya dan sekitarnya. 
+                        Aplikasi didesain untuk memudahkan para pengguna dengan cepat menemukan pusat - pusat daur ulang dan tempat pembuangan sampah.</Text>
+                        
+                    <Text style={styles.text}>
 
-        <View style = {styles.container}>
-            <Text style = {styles.caption}>
-                Stack :
-            </Text>
-            <Text style = {styles.list}>
-                1. React Native 
-            </Text>
-            <Text style = {styles.list}>
-                2. HTML 
-            </Text >
-            <Text style = {styles.list}>
-                3. Leaflet JS 
-            </Text>
-            <Text style = {styles.list}>
-                4. Google Spreadsheet 
-            </Text>
-            <Text style = {styles.list}>
-                5. Font Awesome 
-            </Text>
-            <Text style = {styles.list}>
-                6. Github  
-            </Text>
-        </View>
-        </View>
+                        Fitur Unggulan dari Green Pulse App antara lain : </Text>
+                        
+                    <Text style={styles.text}>
+                        1. Kolaboratif 
+                            Selain mendapatkan informasi mengenai sebaran lokasi pengelolaan sampah di Surabaya dan sekitarnya, 
+                            pengguna juga dapat secara kolaboratif ikut serta dalam menambahkan data dengan fitur yang disajikan oleh Green Pulse App. 
+                            Fitur kolaboratif ini memungkinkan pengguna untuk memberikan pembaruan langsung terkait status tempat pengolahan sampah, jenis sampah yang diterima, 
+                            atau informasi lain yang dapat bermanfaat bagi pengguna lain. </Text>
+
+                    <Text style={styles.text}>
+                        2. Pemetaan Interaktif 
+                            Aplikasi dilengkapi dengan fitur spasial yang memudahkan pengguna dalam menggunakannya. Pengguna dapat menemukan lokasi - lokasi pengelolaan sampah dalam Tab - List Data. 
+                            Pengguna dapat menggunakan fitur Add Data untuk menambahkan data mengenai lokasi bersangkutan. Selanjutnya pengguna dapat menggunakan fitur navigasi dalam list data untuk
+                            mengetahui lebih lengkapi rute menuju lokasi tersebut melalui Google Maps. Bagi pengguna yang memiliki preferensi basemap dari tampilan map yang digunakan,
+                            terdapat fitur 'Change Base Map' yang dapat diatur saat melihat sebaran lokasi bersangkutan.    </Text>
+
+                </View>
+                {/* <View style={styles.container}>
+
+                    <Text style={styles.stack}>Stack : </Text>
+                    <Text style={styles.stack}>1. React Native</Text>
+                    <Text style={styles.stack}>2. HTML</Text>
+                    <Text style={styles.stack}>3. LeafletJS</Text>
+                    <Text style={styles.stack}>4. Google Spreadsheet</Text>
+                    <Text style={styles.stack}>5. Apps Script</Text>
+                    <Text style={styles.stack}>6. FontAwesome5</Text>
+                    <Text style={styles.stack}>7. GitHub</Text>
+                </View> */}
+            </View>
+ 
+        </ScrollView>
     );
 }
 
@@ -67,14 +114,6 @@ function AddDataScreen() {
     );
 }
 
-function ProfileScreen() {
-    return (
-        <View>
-            <Portofolio/>
-        </View>
-    );
-}
-
 function ListScreen() {
     return (
         <View>
@@ -83,10 +122,38 @@ function ListScreen() {
     );
 }
 
+function ProfileScreen() {
+    return (
+        <View>
+            <Portofolio/>
+        </View>
+    );
+}
+
+// function TemanScreen() {
+//     return (
+//         <View>
+//             <Teman/>
+//         </View>
+//     );
+// }
+
 function MyTabs() {
   return (
     <NavigationContainer>
-    <Tab.Navigator screenOptions={{ headerShown: false }} >
+    <Tab.Navigator screenOptions={{ 
+        headerShown: false,
+        tabBarActiveTintColor: '#E2C799', // Warna saat tab aktif
+        tabBarInactiveTintColor: '#F5F5DC', // Warna saat tab tidak aktif
+        tabBarStyle: {
+            bottom: 20, //
+            borderRadius: 25, 
+            left: 20, 
+            right: 20, 
+            height: 50, 
+            position: 'absolute', 
+            backgroundColor: '#004225', // Warna latar belakang tab bar
+    }}} >
         
       <Tab.Screen name="Home" component={HomeScreen} options={{
         tabBarLabel: 'Home',
@@ -98,31 +165,40 @@ function MyTabs() {
       <Tab.Screen name="Map" component={MapsScreen} options={{
         tabBarLabel: 'Map',
         tabBarIcon: ({ color, size }) => (
-              <Fontawesome5 name="map-marked-alt" color={color} size={size}/>
+
+            <Fontawesome5 name="map-marker-alt" color={color} size={size}/>
         ),
         }}
     />
-      <Tab.Screen name="Input Data" component={AddDataScreen} options={{
-        tabBarLabel: 'Input Data',
+      <Tab.Screen name="Add Data" component={AddDataScreen} options={{
+        tabBarLabel: 'Add Data',
         tabBarIcon: ({ color, size }) => (
-              <Fontawesome5 name="plus-circle" color={color} size={size}/>
-        ),
-        }}
-    />
-      <Tab.Screen name="Profile" component={ProfileScreen} options={{
-        tabBarLabel: 'Profile',
-        tabBarIcon: ({ color, size }) => (
-              <Fontawesome5 name="user-circle" color={color} size={size}/>
+            <Fontawesome5 name="plus" color={color} size={size}/>
+
         ),
         }}
     />
       <Tab.Screen name="List Data" component={ListScreen} options={{
         tabBarLabel: 'List',
         tabBarIcon: ({ color, size }) => (
+              <Fontawesome5 name="list-alt" color={color} size={size}/>
+        ),
+        }}
+    />
+        <Tab.Screen name="Profile" component={ProfileScreen} options={{
+        tabBarLabel: 'User',
+        tabBarIcon: ({ color, size }) => (
               <Fontawesome5 name="user-circle" color={color} size={size}/>
         ),
         }}
     />
+        {/* <Tab.Screen name="Teman" component={TemanScreen} options={{
+        tabBarLabel: 'Friends',
+        tabBarIcon: ({ color, size }) => (
+              <Fontawesome5 name="user-circle" color={color} size={size}/>
+        ),
+        }}
+    /> */}
     </Tab.Navigator>
     </NavigationContainer>
   );
@@ -152,15 +228,35 @@ const styles = StyleSheet.create({
         textAlign : 'center',
         marginTop : 10
     },
+    text: {
+        fontSize: 15,
+        textAlign: 'justify',
+
+    },
     container: {
         marginHorizontal : 20,
         marginTop : 20,
         padding : 20,
-        backgroundColor : 'white'
+        backgroundColor : 'white',
+        justifyContent : 'left'
+
     },
+
+    center: {
+        justifyContent : 'center',
+        alignItems : 'center',
+        flex : 1
+
+    },
+    // add: {
+    //     marginBottom : 20,
+    //     },
+    
     card: {
         flexDirection: 'row',
         padding: 20,
+        alignItems : 'center',
+        justifyContent : 'center',
         borderRadius: 10,
         backgroundColor: 'white',
         shadowColor: '#000',
